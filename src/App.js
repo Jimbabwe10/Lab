bc333c584348a133ab74a54e44ee79e5dc8069eb
Jimbabwe10 <156957309+Jimbabwe10@users.noreply.github.com>
2024-03-01 11:59:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1 C2-230L>
-    <h2 Section-002>
-    <p WVU ID800386690>
-    <p Hi I am James>
-    </p>
-    </p>
-    </h2>
-    </h1>
+          <h1 C2-230L>
+              <h2 Section-002>
+                  <p WVU ID800386690>
+                      <p Hi I am James>
+                      </p>
+                  </p>
+              </h2>
+          </h1>
     </div>
   );
 }
