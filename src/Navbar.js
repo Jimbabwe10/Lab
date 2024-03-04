1f7c5118e0ccef14882c28c2d2cacefce6b99fa2
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-04 15:28:41 -0500

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,8 +12,10 @@ const CustomNavbar = () => {
                 <Nav href="#">Contact</Nav>
                 </form>
             </Nav>
-                <input class="form-control mr-sm-1" ype="search" placeholder="Search" aria-label="Search"></input>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </Navbar>
     );
 };
