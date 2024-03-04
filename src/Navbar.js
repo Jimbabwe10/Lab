8a7886880ddd47de06d4b76411d26d723ebdb2c8
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-03 22:18:33 -0500

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,6 +9,7 @@ const CustomNavbar = () => {
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#About">About</Nav.Link>
                 <Nav.Link href="#Contact">Contact</Nav.Link>
+                <a class="navbar-brand">Navbar</a>
                 <form class="form-inline">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
