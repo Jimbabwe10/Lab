323a0ea83ea846c2d1e5abb4df57163b90c951bf
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-03 22:46:12 -0500

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const CustomNavbar = () => {
     return (
-        <Nav class="bg-dark variant-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#">Navbar</Navbar.Brand>
             <Nav class="navbar navbar-dark bg-dark text-light justify-content-between">
                 <form class="form-inline">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
+                <Nav.Link href="#">Home</Nav.Link>
+                <Nav.Link href="#">About</Nav.Link>
+                <Nav.Link href="#">Contact</Nav.Link>
                 </form>
             </Nav>
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </Nav>
+        </Navbar>
     );
 };
 
