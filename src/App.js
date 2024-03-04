a1238407e98bdeb3b7ac5322d125d867c7851d89
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-03 19:29:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import './Card.js';
-import './Navbar.js';
+import CustomCards from './Card.js';
+import CustomNavbar from './Navbar.js';
 
 function App() {
   return (
       <div className="App">
-          <h1> C2-230L </h1>
+          <CustomNavbar />
+           <CustomCards />
+            <h1> C2-230L </h1>
               <h2> Section-002 </h2>
                   <p> WVU ID: 800386690 </p>
                       <p> Hi I am James </p>
