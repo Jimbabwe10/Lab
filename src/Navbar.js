9be259d13b1469e0fc72ddd36a4d5a6f570ffa40
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-03 22:07:31 -0500

    Update Components

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,8 +10,9 @@ const CustomNavbar = () => {
                 <Nav.Link href="#About">About</Nav.Link>
                 <Nav.Link href="#Contact">Contact</Nav.Link>
                 <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </input>
                 </form>
             </Nav>
         </Navbar>
