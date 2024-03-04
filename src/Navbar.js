9d4a947c0de23916396a50c199bdb1dcbe1693e4
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-03 22:29:13 -0500

    Update Components

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,15 +5,15 @@ const CustomNavbar = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav class="navbar navbar-dark bg-dark">
+            <Nav class="navbar navbar-dark bg-dark justify-content-between">
                 <form class="form-inline">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#About">About</Nav.Link>
-                <Nav.Link href="#Contact">Contact</Nav.Link>
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <Nav.Link href="#Contact">Contact</Nav.Link>
                 </form>
             </Nav>
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </Navbar>
     );
 };
