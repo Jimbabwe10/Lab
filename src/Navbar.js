3d1a97127046b59a83bb31de641880fa79161cd8
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-04 15:20:47 -0500

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,17 +3,20 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 const CustomNavbar = () => {
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#">Navbar</Navbar.Brand>
-            <Nav class="navbar navbar-dark bg-dark text-light justify-content-between">
-                <form class="form-inline">
-                <Nav.Link href="#">Home</Nav.Link>
-                <Nav.Link href="#">About</Nav.Link>
-                <Nav.Link href="#">Contact</Nav.Link>
-                </form>
-            </Nav>
-                <input class="form-control mr-sm-1" ype="search" placeholder="Search" aria-label="Search"></input>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <Navbar class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+        <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Features</a>
+            <a class="nav-item nav-link" href="#">Pricing</a>
+        </div>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </Navbar>
     );
 };
